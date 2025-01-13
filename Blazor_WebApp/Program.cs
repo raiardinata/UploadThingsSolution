4535e58d5f6ecd6d1498fe15e9cbbecaa0f856e2
raiardinata <raiardinata@gmail.com>
2025-01-13 16:42:14 +0700

    Integrating Blazor with gRpc (with broken css)

--- Blazor_WebApp/Program.cs
+++ Blazor_WebApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazor_WebApp.Application.Services;
 using Blazor_WebApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHttpClient<PizzaService>();
 
 var app = builder.Build();
 
