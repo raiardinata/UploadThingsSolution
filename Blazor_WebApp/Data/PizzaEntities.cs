65c8b34a5c17556caff2c82cc44f9e3ea72e5b2e
raiardinata <raiardinata@gmail.com>
2025-01-13 16:42:14 +0700

    Integrating Blazor with gRpc (with broken css)
    Add PizzaSpecial TestCase

--- Blazor_WebApp/Data/PizzaEntities.cs
+++ Blazor_WebApp/Data/PizzaEntities.cs
@@ -1,12 +1,18 @@
 ﻿namespace Blazor_WebApp.Data
 {
+    // Outer response class
+    public class PizzaSpecialResponse
+    {
+        public List<PizzaEntities> PizzaSpecialData { get; set; } = new List<PizzaEntities>();
+    }
+
     public class PizzaEntities
     {
-        public int PizzaID { get; set; }
-        public string? PizzaName { get; set; }
-        public string? PizzaDescription { get; set; }
-        public decimal PizzaPrice { get; set; } = 0;
-        public bool Vegetarian { get; set; }
-        public bool Vegan { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal BasePrice { get; set; } = 0;
+        public string? ImageUrl { get; set; }
+        public string GetFormattedBasePrice() => BasePrice.ToString("0.00");
     }
 }
